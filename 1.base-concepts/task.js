5e2ca1509caced5f125880d4d9f44418b8618713
Kardinal Francisk <andern1990@gmail.com>
2023-06-17 22:36:43 +0300

    Первое задание сделано

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,9 +1,18 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  let x;
-  
-  return arr;
+  let d = Math.pow(b,2) - 4*a*c;
+  let x = (-b+Math.sqrt(d))/(2*a);
+  let y = (-b-Math.sqrt(d))/(2*a);
+  if (d<0) {
+   return arr; 
+  } else if (d===0){
+    x= -b/(2*a);
+    arr.push(x);
+  }else {
+    arr.push(x,y);
+  }
+ return arr; 
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
